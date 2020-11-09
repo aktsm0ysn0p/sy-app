8d12250fd7c9ef857ce96835e9be5f44cfcf64f3
Aika Takada <aktsm0syn0pgmail.com>
2020-11-09 12:14:51 +0900

    VStockのデータを戻すために一時保存

---
+++ src/pages/Main/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="lists-page">
+    <TheHeader />
+    <nav>
+      <ul>
+        <li><button @click="tabClick(1)" :class="{active : isSelect === 1}">●</button></li>
+        <li><button @click="tabClick(2)" :class="{active : isSelect === 2}">☆</button></li>
+      </ul>
+    </nav>
+    <div class="container">
+      <div class="quote-wrapper">
+        <div class="quote-card" v-for="list in lists" :key= "list.id">
+          <h3>{{list.title}}</h3>
+          <span @click="setStock(list.id)" :class="{nowstock : folders.find(folder=> folder.id === list.id)}">○</span>
+          <p>{{list.since}}{{list.name}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import TheHeader from './components/TheHeader';
+// import TheLists from './components/TheLists';
+
+
+export default {
+  components: {
+    TheHeader,
+  },
+  data() {
+    return {
+      isStock: false,
+      lists: [
+        {
+          id: 1,
+          title: "吾輩は猫である",
+          name: "夏目漱石",
+          since: "1996 ~ 2000"
+        },
+        {
+          id: 2,
+          title: "明日はきっと晴れるよ",
+          name: "高田愛佳",
+          since: "1996 ~ 1998"
+        },
+        {
+          id: 3,
+          title: "2番じゃだめなんですか？",
+          name: "蓮舫",
+          since: "889 ~ 1000"
+        },
+        {
+          id: 4,
+          title: "ちっちゃいことは気にすんな",
+          name: "ゆってぃー",
+          since: "2010 ~ 2011"
+        },
+      ],
+      folders: [
+        {id: 3},
+      ],
+    }
+  },
+  methods: {
+    setStock(id) {
+      const target = this.folders.find( folder => folder.id === id);
+      if (target) {
+        // 既にストックされていたらtrue stockから削除する
+        console.log(`tureらしいよ!`);
+        const newfolers = this.folders.filter(folder => folder.id !== id);
+        console.log(newfolers);
+        this.folders = newfolers;
+      } else {
+        //ストックされていなかったらfalse 追加してあげる
+        console.log('falseらしいよ');
+        const stock = {id: id};
+        this.folders.push(stock);
+      }
+    },
+  }
+
+}
+</script>
+
+<style>
+
+.lists-page {
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.container {
+  width: 90%;
+  margin: 10vh auto;
+}
+.quote-card {
+  padding: 20px;
+  margin: 10px 0;
+  border: solid 1px #5ABD57;
+  background-color: #A9F791;
+}
+h3 {
+  text-align: center;
+  font-size: .8rem;
+}
+p {
+  float: right;
+  font-size: .8em;
+}
+span {
+  cursor: pointer;
+}
+.nowstock {
+  font-weight: bold;
+  background: pink;
+}
+
+</style>
---
+++ src/pages/Main/stock.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <TheHeader />
+    <nav>
+      <ul>
+        <li><button @click="tabClick(1)" :class="{active : isSelect === 1}">●</button></li>
+        <li><button @click="tabClick(2)" :class="{active : isSelect === 2}">☆</button></li>
+      </ul>
+    </nav>
+    <!-- <BaseLayout :active="isSelect" /> -->
+    <div class="container">
+      <div class="inner">
+        <span class="inner-title">ストック一覧</span>
+        <div class="inner-folder">
+          <div v-if="folders.length">
+            <div class="quote-card" v-for="meigenn in meigenns" :key= "meigenn.id">
+              <h3>{{meigenn.title}}</h3>
+              <span @click="deleStock(meigenn.id)" :class="{nowstock : meigenn.stock}">削除</span>
+              <p>{{meigenn.since}}{{meigenn.name}}</p>
+            </div>
+          </div>
+          <p v-else>まだ何もありません</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import TheHeader from './components/TheHeader';
+// import VStock from './components/VStock';
+
+
+export default {
+  components: {
+    TheHeader,
+  },
+  props: {
+    lists: Array,
+    folders: Array
+  },
+  data() {
+    return {
+      myStockFolders: []
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    deleStock(id) {
+      const stock = {id: id};
+      this.$emit('emiting', stock);
+    }
+  }
+
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+.container {
+  width: 90%;
+  margin: 10vh auto;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.active {
+  color: red;
+}
+nav {
+  text-align: center;
+}
+nav > ul {
+  display: flex;
+  justify-content: space-around;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+button{
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  appearance: none;
+}
+</style>
