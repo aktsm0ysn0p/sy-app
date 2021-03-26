9f52b4076a35230b2af2bcbd9caf75738caabf29
Aika Takada <aktsm0syn0pgmail.com>
2021-03-26 15:02:24 +0900

    タイポ修正

--- src/pages/Login/signIn.vue
+++ src/pages/Login/signIn.vue
@@ -47,7 +47,7 @@ export default {
       this.$store.dispatch('MyQuotes/initUser')
     },
     signIn() {
-      this.$store.dispatch('MyQuoters/signIn')
+      this.$store.dispatch('MyQuotes/signIn')
       .then(() => {
           this.$router.push({ path: '/' })
       })
